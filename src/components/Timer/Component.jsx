9831a784bcd50d28eb069ad2eba39d9b89ab6b5c
Zack Tillotson <ZackTillotson@gmail.com>
2021-03-30 12:38:30 -0600

    Updates timer with more info and styles, also allows user to click to hide the clock

--- src/components/Timer/Component.jsx
+++ src/components/Timer/Component.jsx
@@ -23,27 +23,29 @@ function getEventTimes(events) {
 }
 
 function getTimeSince(startTime = 0, endTime = 0) {
-  if(!startTime) return '00:00:00'
+  if(!startTime) return '00:00'
 
   const length = endTime - startTime
   const minutes = Math.trunc(Number(length / 1000 / 60))
   const seconds = Math.trunc(Number(length - minutes * 1000 * 60) / 1000)
-  const milliseconds = Math.trunc(Number(length - minutes * 1000 * 60 - seconds * 1000) / 10)
-  return `${minutes}`.padStart(2, '0') + ':' + `${seconds}`.padStart(2, '0')  + ':' + `${milliseconds}`.padStart(2, '0')
+  return `${minutes}`.padStart(2, '0') + ':' + `${seconds}`.padStart(2, '0') 
 }
 
 function Component({events, players}) {
 
   const lastEvent = [...events].reverse()[0] || null
+  const player = lastEvent ? players.find(player => player.id === lastEvent.who) : null
 
   const [clock, updateClock] = useState()
   useEffect(() => {
     updateClock(getTimeSince(...getEventTimes(events)))
     const clockInterval = setInterval(() => {
       updateClock(getTimeSince(...getEventTimes(events)))
-    }, 10)
+    }, 1000)
     return () => clearInterval(clockInterval)
   }, [events])
+
+  const [isClockVisible, updateIsClockVisible] = useState(true)
 
   let borderColor = COLOR_DEFAULT
   switch(true) {
@@ -53,14 +55,23 @@ function Component({events, players}) {
     break
 
     case (lastEvent && lastEvent.type === 'TURN_START'): {
-      borderColor = players.find(player => player.id === lastEvent.who).color
+      borderColor = player.color
     }
     break
   }
 
+  const turnCount = events.filter(event => player && event.who === player.id).length
+
+  const handleClick = event => {
+    updateIsClockVisible(!isClockVisible)
+  }
+
   return (
-    <div className="timer" style={{borderColor}}>
-      {clock}
+    <div className={cn('timer', {['timer--no-clock']: !isClockVisible})} style={{borderColor}} onClick={handleClick}>
+      <div className="timer__player">{!lastEvent && 'Click player to start' || lastEvent.type === 'END' && 'Click "View stats" for game overview' || !player && '-' || player.name}</div>
+      <div className="timer__clock">{clock}</div>
+      <div className="timer__round">Round {Math.trunc((events.length - 1) / players.length) + 1}</div>
+      <div className="timer__turn">{turnCount && `Turn ${turnCount}` || '-'}</div>
     </div>
   );
 }
