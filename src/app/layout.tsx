2205653ca113067c517ca7061e3611a80eccaf00
Zack Tillotson <Zachery.Tillotson@cvshealth.com>
2024-08-08 14:22:26 -0600

    Adds Mantine component library with basic theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,16 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { 
+  ColorSchemeScript, 
+  MantineProvider, 
+} from '@mantine/core'
+import {theme} from '@/theme/'
+
+import { Inter } from "next/font/google"
+
+import "./_styles/reset.scss"
+import "./_styles/mantine.scss"
+import "./_styles/globals.scss"
+import '@mantine/core/styles.css'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+        <head>
+          <ColorSchemeScript />
+        </head>
+        <body className={inter.className}>
+          <MantineProvider theme={theme}>
+            {children}
+          </MantineProvider>    
+        </body>
+      </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
+
+import Link from 'next/link';
+import { Button, Stack } from '@mantine/core';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Who Slow v2</h1>
-      <h2>About</h2>
-      <p>Playing board games? Find out who the slow players are with Who Slow - an app for tracking player turn lengths.</p>
+      <Stack>
+        <h1>Who Slow v2</h1>
+        <h2>About</h2>
+        <p>Playing board games? Find out who the slow players are with Who Slow - an app for tracking player turn lengths.</p>
+        <Button component={Link} href="/app/" variant="filled">App</Button>
+      </Stack>
     </main>
   );
 }
