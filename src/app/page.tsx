a837adbb02654528cec7c5b323875f7c2e857ce5
Zack Tillotson <Zachery.Tillotson@cvshealth.com>
2024-08-09 09:31:01 -0600

    Adds image assets from master branch. Styles homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,47 @@
-import styles from "./page.module.scss";
-
 import Link from 'next/link';
-import { Button, Stack } from '@mantine/core';
+import { 
+  Text,
+  Button,
+  Stack,
+  Container,
+  Flex,
+} from '@mantine/core';
+import Image from 'next/image'
 
-export default function Home() {
+import styles from "./page.module.scss";
+import hero from '@/assets/logo-400x400.png'
+
+export default function Landing() {
   return (
-    <main className={styles.main}>
-      <Stack>
-        <h1>Who Slow v2</h1>
-        <h2>About</h2>
-        <p>Playing board games? Find out who the slow players are with Who Slow - an app for tracking player turn lengths.</p>
-        <Button component={Link} href="/app/" variant="filled">App</Button>
-      </Stack>
-    </main>
+    <Flex
+      className={styles.container}
+      gap="sm"
+      direction="column"
+      wrap="nowrap"
+      px="md"
+      py="xl"
+    >
+      <div className={styles.hero}>
+        <Image
+          src={hero}
+          className={styles.heroImage}
+          width="400"
+          height="400"
+          alt="Beautiful turtle with 'Who Slow' logo"
+        />
+      </div>
+      <div className={styles.explanation}>
+        <h1 className="subtitle">Board game stats and turn timer</h1>
+        <p>Playing a board game campaign? Have a group with slow players? Use this fun app to keep track of stats and definitively answer - Who Slow?</p>
+      </div>
+      <div className={styles.controls}>
+        <Button
+          component={Link}
+          href="/app/"
+        >
+          Get started
+        </Button>
+      </div>
+    </Flex>
   );
 }
