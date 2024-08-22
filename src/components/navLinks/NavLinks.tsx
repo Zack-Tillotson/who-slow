bd0ff8dce429301bb97f303b7bf9720f4977a769
Zack Tillotson <Zachery.Tillotson@cvshealth.com>
2024-08-22 14:24:44 -0600

    Cleans up UI

--- src/components/navLinks/NavLinks.tsx
+++ src/components/navLinks/NavLinks.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from '@mantine/core'
-import {IconMapCog, IconUsers, IconDice5, IconHome} from '@tabler/icons-react'
+import {IconMapCog, IconUsers, IconDice5, IconHome, IconChevronRight} from '@tabler/icons-react'
 
 interface NavLinksProps {
   withHome?: boolean,
@@ -13,17 +13,17 @@ const navLinks = [{
 }, {
   href: '/campaign/', 
   title: 'Campaigns', 
-  desc: 'View and manage campaigns', 
+  desc: 'View and manage campaigns, a series of game sessions that share stats.', 
   IconImage: IconMapCog,
 }, {
   href: '/player/', 
   title: 'Players', 
-  desc: 'Configure the players of your campaign', 
+  desc: 'Configure players. Enter new players here when they are part of a session.', 
   IconImage: IconUsers,
 }, {
   href: '/game/', 
   title: 'Games', 
-  desc: 'Configure the games you play', 
+  desc: 'Configure the games you play. Link with BoardGameGeek to get detailed info.', 
   IconImage: IconDice5,
 }
 ]
@@ -40,6 +40,7 @@ export function NavLinks({withHome = false}: NavLinksProps) {
             label={title}
             description={desc}
             leftSection={<IconImage size="1rem" stroke={1.5} />}
+            rightSection={<IconChevronRight />}
           />
         ))}
     </>
--- src/views/sessions/Session.tsx
+++ src/views/sessions/Session.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useDataState } from "@/state";
-import { Button, Group, Pill, Text, Title } from "@mantine/core";
+import { Button, Divider, Group, Paper, Pill, Stack, Text, Title } from "@mantine/core";
 import Link from "next/link";
 
 type ViewProps = {
@@ -20,22 +20,53 @@ export function Session({sessionId}: ViewProps) {
 
   if(!session) {
     return (
-      <h1>Error: game not found</h1>
+      <div>
+        <Title order={1}>Error</Title>
+        <Text>Session not found</Text>
+      </div>
     )
   }
 
+  const status = getSessionStatusText(session)
+
   return (
-    <>
-      <div>
-        <Group>
-          <Title order={3} flex={1}>{new Date(session.date).toLocaleDateString()}</Title>
+    <Paper withBorder shadow="md" p="sm" mt="lg">
+      <Group>
+        <Title order={3} flex={1} size="md">Session</Title>
+        {status === 'Not started' && (
           <Button component={Link} href={`/session/${session.id}/edit`} variant="subtle">Edit</Button>
-          <Pill bg={session.status === 'POST' ? 'lightgreen' : ''}>{getSessionStatusText(session)}</Pill>
-        </Group>
-        <Text>Playing {getGame(session.game)?.name}</Text>
-        <Text>{session.sessionPlayers.map(({player}) => (getPlayer(player)?.name ?? 'Player')).join(', ')}</Text>
-      </div>
-      <Button component={Link} href={`/session/${session.id}/play`}>Begin game</Button>
-    </>
+        )}
+        <Pill bg={status === 'Session ended' ? 'lightgreen' : ''}>{status}</Pill>
+      </Group>
+      <Stack gap="0">
+        <Text size="xs">Date</Text>
+        <Text size="xs" fw={700}>{new Date(session.date).toLocaleDateString()}</Text>
+      </Stack>
+      <Stack gap="0">
+        <Text size="xs">Game</Text>
+        <Text size="xs" fw={700}>{getGame(session.game)?.name}</Text>
+      </Stack>
+      <Stack gap="0">
+        <Text size="xs">Players</Text>
+        <Text size="xs" fw={700}>{session.sessionPlayers.map(({player}) => (getPlayer(player)?.name ?? 'Player')).join(', ')}</Text>
+      </Stack>
+      <Divider mt="sm" />
+      <Group pt="md">
+        <Button
+          component={Link}
+          href={`/session/${session.id}/play`}
+          variant={status !== 'Session ended' ? "filled" : "subtle"}
+        >
+          Start session
+        </Button>
+        <Button
+          component={Link}
+          href={`/session/${session.id}/stats`}
+          variant={status === 'Session ended' ? "filled" : "subtle"}
+        >
+          View stats
+        </Button>
+      </Group>
+    </Paper>
   )
 }
