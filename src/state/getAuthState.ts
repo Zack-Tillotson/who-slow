5b9bbae485ecb72ce7bffcc9a8cee26d27594642
Zack Tillotson <Zachery.Tillotson@cvshealth.com>
2025-01-08 13:36:46 -0700

    Adds ssr delay if auth not resolved

--- src/state/getAuthState.ts
+++ src/state/getAuthState.ts
@@ -3,12 +3,14 @@ import firebase from "./firebase"
 
 export default async () => {
   const headerList = headers()
-  const idToken = headerList.get('Authorization')?.split('Bearer ')[1]
-
+  const idToken = headerList.get('Authorization')?.split('Bearer ')[1] || ''
   const auth = firebase(idToken).getAuth()
   await auth.authStateReady()
+  if(!auth.currentUser) {
+    await new Promise(resolve => setTimeout(resolve, 10))
+  }
 
-  console.log('INFO', 'getAuthState', 'idToken?', !!idToken, 'await authStateReady().currentUser?', !!auth.currentUser)
+  console.log('INFO', 'getAuthState', 'await authStateReady().currentUser?', !!auth.currentUser)
 
   return auth
 }
