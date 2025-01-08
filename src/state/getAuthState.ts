da2bb27c572431041d85f247e0b821b5e92ebcca
Zack Tillotson <Zachery.Tillotson@cvshealth.com>
2025-01-08 16:09:11 -0700

    Always create a new connection if the idToken is passed in

--- src/state/getAuthState.ts
+++ src/state/getAuthState.ts
@@ -6,11 +6,15 @@ export default async () => {
   const idToken = headerList.get('Authorization')?.split('Bearer ')[1] || ''
   const auth = firebase(idToken).getAuth()
   await auth.authStateReady()
-  if(!auth.currentUser) {
-    await new Promise(resolve => setTimeout(resolve, 10))
-  }
-
-  console.log('INFO', 'getAuthState', 'await authStateReady().currentUser?', !!auth.currentUser)
+  
+  console.log(
+    'INFO', 
+    'getAuthState post authStateReady', 
+    'idToken?', 
+    !!idToken, 
+    'auth.currentUser?', 
+    !!auth.currentUser
+  )
 
   return auth
 }
